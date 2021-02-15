305c78f330bd8fe483ab094b2c56685716bf4e64
Kirill Kolodiy <kirill.kolodiy@gmail.com>
2021-02-15 19:07:50 +0300

    changed index and script files

---
+++ script.js
@@ -0,0 +1,12 @@
+const rows = 10;
+const columns = 10;
+let mines, remaining, revealed;
+
+let board = new Array(rows);
+let picture = new Array(rows);
+let tile = new Array(rows);
+for (let i = 0; i < board.length; i++) {
+  board[i] = new Array(columns);
+  picture[i] = new Array(columns);
+  tile[i] = new Array(columns)
+}
