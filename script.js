0d837b7fc8558b36c3ca0aa7f3ea410be57e1374
Kirill Kolodiy <kirill.kolodiy@gmail.com>
2021-02-15 22:20:20 +0300

    update init function

--- script.js
+++ script.js
@@ -39,3 +39,76 @@ function init() {
       picture[row][column] = 'hidden';
       board[row][column] = '';
     }
+
+  let placed = 0;
+  while (placed < mines) {
+    let column = Math.floor(Math.random() * columns);
+    let row = Math.floor(Math.random() * rows);
+
+    if (board[row][column] != 'mine') {
+      board[row][column] = 'mine';
+      placed++;
+    }
+  }
+
+  for (let column = 0; column < columns; column++)
+    for (let row = 0; row < rows; row++) {
+      if (check(row, column) != 'mine') {
+        board[row][column] =
+          ((check(row + 1, column) == 'mine') | 0) +
+          ((check(row + 1, column - 1) == 'mine') | 0) +
+          ((check(row + 1, column + 1) == 'mine') | 0) +
+          ((check(row - 1, column) == 'mine') | 0) +
+          ((check(row - 1, column - 1) == 'mine') | 0) +
+          ((check(row - 1, column + 1) == 'mine') | 0) +
+          ((check(row, column - 1) == 'mine') | 0) +
+          ((check(row, column + 1) == 'mine') | 0);
+      }
+    }
+}
+
+function click(event) {
+  let source = event.target;
+  let id = source.id;
+  let row = Math.floor(id / columns);
+  let column = id % columns;
+
+  if (event.which == 3) {
+    switch (picture[row][column]) {
+      case 'hidden':
+        tile[row][column].src = 'img/flag.png';
+        remaining--;
+        picture[row][column] = 'flag';
+        break;
+      case 'flag':
+        tile[row][column].src = 'img/question.png';
+        remaining++;
+        picture[row][column] = 'question';
+        break;
+      case 'question':
+        tile[row][column].src = 'img/hidden.png';
+        picture[row][column] = 'hidden';
+        break;
+    }
+    event.preventDefault();
+  }
+  status.innerHTML = 'Mines remaining: ' + remaining;
+
+  if (event.which == 1 && picture[row][column] != 'flag') {
+    if (board[row][column] == 'mine') {
+      for (let row = 0; row < rows; row++)
+        for (let column = 0; column < columns; column++) {
+          if (board[row][column] == 'mine') {
+            tile[row][column].src = 'img/mine.png';
+          }
+          if (board[row][column] != 'mine' && picture[row][column] == 'flag') {
+            tile[row][column].src = 'img/misplaced.png';
+          }
+        }
+      status.innerHTML = 'GAME OVER<br><br>Click here to restart';
+    } else
+    if (picture[row][column] == 'hidden') reveal(row, column);
+  }
+
+  if (revealed == rows * columns - mines)
+    status.innerHTML = 'YOU WIN!<br><br>Click here to restart';
